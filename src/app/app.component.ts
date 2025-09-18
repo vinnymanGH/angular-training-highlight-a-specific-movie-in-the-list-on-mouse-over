f87246391f3cb92c46d0ede293cbd48885be3a92
Vince Boisvert <vinceboisvert@gmail.com>
2025-09-18 22:24:42 +0000

    Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, Signal} from '@angular/core';
 import {MovieItemComponent} from './movie-item/movie-item.component';
 import {Movie} from './model/movie.model';
 import {MoviesService} from './services/movies.service';
+import {HighlightDirective} from './highlight.directive';
 
 
 @Component({
@@ -10,7 +11,7 @@ import {MoviesService} from './services/movies.service';
 
   templateUrl: 'app.component.html',
   imports: [
-    MovieItemComponent
+    MovieItemComponent, HighlightDirective
   ]
 })
 export class AppComponent {
